0d10ac19598b7fe7f9c296189c8cfc5c445af2ae
mayi-oi <arkadlen@proton.me>
2026-07-24 16:15:27 +0200

    Erste version den Website sind hier! :D
    wow ist sehr cool hier!
    und für touch screen nutzter ermmmm.... nicht besuche bitte

---
+++ Java/toggle.js
@@ -0,0 +1,14 @@
+function Ds_filter() {
+    const dropdown = document.getElementById('filtermenu');
+    if (dropdown) dropdown.classList.toggle('open');
+}
+
+function Ds_modandaddons() {
+    const dropdown = document.getElementById('Mods-und-addons');
+    if (dropdown) dropdown.classList.toggle('open');
+}
+
+function Ds_grid() {
+    const dropdown = document.getElementById('Grid-oder-list');
+    if (dropdown) dropdown.classList.toggle('open');
+}
